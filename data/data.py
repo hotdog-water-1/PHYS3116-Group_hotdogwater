d8c5060fad52c162335528eb98ab341e9b3da168
hotdog-water-1 <tehtemmie7654@gmail.com>
2026-09-26 02:54:26 +1000

    Added ellip filter. trying to plot data/relation on log scale

--- data/data.py
+++ data/data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
 
 photodata = pd.read_csv("data/samiDR3InputCatClusters.csv")
 skinedata = pd.read_csv("data/samiDR3StelKin.csv")
@@ -9,14 +10,25 @@ unified = photodata \
     .merge(skinedata.set_index("catid"), on="catid") \
     .merge(visualdata.set_index("catid"), on="catid").set_index("catid") \
     .loc[lambda df: df["bad_class"] == 0] \
-    .loc[lambda df: df["sigma_re_err"] <= 5]
-    #.loc[lambda df: df["type"] <= 0]
+    .loc[lambda df: df["sigma_re_err"] <= 5] \
+    .loc[lambda df: df["ellip"] < 0.5]
+    # .loc[lambda df: df["type"] <= 0]
 
 print(unified)
 
 fig, ax = plt.subplots()
-ax.scatter(unified["m_r"], unified["sigma_re"], s = 5)
+z = np.polyfit(unified["m_r"], np.log10(unified["sigma_re"]), 1)
+p = np.poly1d(z)
+ax.plot(unified["m_r"], p(unified["m_r"]))
+#
+# ax.scatter(unified["m_r"], unified["sigma_re"], s = 5)
+ax.scatter(unified["m_r"], np.log10(unified["sigma_re"]), s = 5)
 ax.invert_xaxis()
+
+plt.xlabel("$M_r$ (mags)")
+# plt.ylabel("$\sigma$ (km/s)")
+plt.ylabel("log $\sigma$ (km/s)")
+plt.legend()
 plt.show()
 # 
 # print(photodata["m_r"])
